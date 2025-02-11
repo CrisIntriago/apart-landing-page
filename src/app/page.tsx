8a2bff8003b9dec44a6e0598670d513e9b2e4b36
CrisIntriago <caintria@espol.edu.ec>
2025-02-10 23:22:37 -0500

    apart pestañas pre view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-"use client";
-import { useState } from "react";
 import Navbar from "../components/navbar"; // Asegúrate de tener el Navbar importado
 import Hero from "@/sections/hero";
 
 export default function Home() {
-  const [isVideoReady, setIsVideoReady] = useState(false);
-
-  // Función que se ejecuta cuando el video puede comenzar a reproducirse
-  const handleCanPlay = () => {
-    setIsVideoReady(true); // Video listo para reproducirse
-  };
-
   return (
     <div>
-      <Hero />
-      <Navbar className="absolute top-0 left-0 w-full z-10" />
+      <Navbar/>
+      {/* <Hero /> */}
     </div >
   );
 }
