2d9d3154bda1f27ed19a0e79e92f21f46beaadad
CrisIntriago <caintria@espol.edu.ec>
2025-02-14 20:03:30 -0500

    forbes section done

--- src/sections/forbes_section.tsx
+++ src/sections/forbes_section.tsx
@@ -1,15 +1,15 @@
 export default function ForbesSection() {
     return (
-        <section className="flex flex-col items-center justify-center w-full bg-white text-black h-80 py-16 relative">
+        <section className="flex flex-col items-center justify-center w-full bg-white text-black h-[500px] py-16 relative">
             {/* Imagen encima del texto */}
             <img 
                 src="/images/forbes_logo.png" 
                 alt="forbes_logo" 
-                className="w-32 h-auto" 
+                className="h-8 w-auto" 
             />
             
             {/* Texto */}
-            <p className="text-5xl relative z-10">
+            <p className="text-5xl text-center relative z-10 py-8 px-20">
                 “Dominar el inglés y el español incrementa hasta casi
                 cuatro veces tus ingresos.”
             </p>
