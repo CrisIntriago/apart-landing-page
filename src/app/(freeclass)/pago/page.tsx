92f33d3b810cb0cee403ba61cccec9b72bfcd13d
CrisIntriago <caintria@espol.edu.ec>
2025-04-14 22:55:57 -0500

    suspense needed fix

--- src/app/(freeclass)/pago/page.tsx
+++ src/app/(freeclass)/pago/page.tsx
@@ -1,12 +1,12 @@
-
-"use client"
+"use client";
 import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation"; // Use useSearchParams from next/navigation
 import PaymentForm from "@/components/PaymentForm";
 import CarrouselPayment from "@/components/CarrouselPayment";
 import { paymentTypes, PaymentType } from "@/constants/constants";
 
-const FreeClass = () => {
+// Client-side component that uses search params
+const FreeClassContent = () => {
   const searchParams = useSearchParams(); // Access query parameters
   const oferta = searchParams.get("oferta");
 
@@ -40,20 +40,23 @@ const FreeClass = () => {
   }
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className="flex flex-col min-h-screen h-screen bg-[#FAF5DA]">
-        <h1 className="bg-black w-full lg:px-32 py-3 text-4xl font-bold text-[#FAF5DA]">
-          <a href="/">APART</a>
-        </h1>
-        <div className="flex sm:flex-row h-full">
-          <Suspense fallback={<div>Loading Carousel...</div>}>
-            <CarrouselPayment />
-          </Suspense>
-          <Suspense fallback={<div>Loading Payment Form...</div>}>
-            <PaymentForm paymentType={selectedPaymentType} />
-          </Suspense>
-        </div>
+    <div className="flex flex-col min-h-screen h-screen bg-[#FAF5DA]">
+      <h1 className="bg-black w-full lg:px-32 py-3 text-4xl font-bold text-[#FAF5DA]">
+        <a href="/">APART</a>
+      </h1>
+      <div className="flex sm:flex-row h-full">
+        <CarrouselPayment />
+        <PaymentForm paymentType={selectedPaymentType} />
       </div>
+    </div>
+  );
+};
+
+// Main component that wraps the client-side component in Suspense
+const FreeClass = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <FreeClassContent />
     </Suspense>
   );
 };
